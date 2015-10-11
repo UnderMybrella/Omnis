3adf1770cdb2046b88e6251820fa5f41f761bb91
Isaac <undermybrella@abimon.org>
2015-10-11 23:00:20 +1100

    Added the Foreground layer variable. The layer variables are currently testing values, and may not be final Let the GameWindowRepaintThread sleep for half a second each time. Added a few more tiles and took out some of the debug data

--- src/org/abimon/omnis/ludus/multithreading/GameWindowRepaintThread.java
+++ src/org/abimon/omnis/ludus/multithreading/GameWindowRepaintThread.java
@@ -3,16 +3,20 @@ package org.abimon.omnis.ludus.multithreading;
 import org.abimon.omnis.ludus.GameWindow;
 
 public class GameWindowRepaintThread extends Thread {
-	
+
 	GameWindow window;
-	
+
 	public GameWindowRepaintThread(GameWindow window){
 		this.window = window;
 	}
-	
+
 	public void run(){
 		while(true){
-			window.repaint();
+			try{
+				window.repaint();
+				Thread.sleep(500);
+			}
+			catch(Throwable th){}
 		}
 	}
 }
--- src/org/abimon/omnis/nhm/NagitosHopeMachine.java
+++ src/org/abimon/omnis/nhm/NagitosHopeMachine.java
@@ -2,7 +2,6 @@ package org.abimon.omnis.nhm;
 
 import java.io.File;
 
-import org.abimon.omnis.io.Data;
 import org.abimon.omnis.ludus.Floor;
 import org.abimon.omnis.ludus.LayerList;
 import org.abimon.omnis.ludus.Ludus;
@@ -10,21 +9,20 @@ import org.abimon.omnis.ludus.Tile;
 
 public class NagitosHopeMachine {
 	
-	public static Tile grass = new Tile("NHM:GrassSpiral", "grass_spiral", "resources/GrassSpiral.png");
+	public static Tile air = new Tile("NHM:air", "air", "");
+	public static Tile grass = new Tile("NHM:grass", "grass", "resources/Grass.png");
+	public static Tile rock = new Tile("NHM:rock", "rock", "resources/Rock.png");
 	
 	public static void main(String[] args){
 		Ludus.registerDataPool(NagitosHopeMachine.class.getClassLoader());
 		Ludus.registerDataPool(new File("resources"));
-		Data data = Ludus.getDataUnsafe("resources/GrassSpiral.png");
-		System.out.println(data);
 		
 		Ludus.reloadIcons();
 		
 		Floor floor = new Floor("Floor #1");
 		floor.setLayer(LayerList.BACKGROUND_LAYER, new Tile[][]{{grass, grass, grass}, {grass, grass, grass}, {grass, grass, grass}});
-		Floor copy = floor.clone();
-		System.out.println(floor + "\n" + copy + "\n" + floor.equals(copy));
-		
+		floor.setLayer(LayerList.FOREGROUND_LAYER, new Tile[][]{{air, air, air}, {air, rock, air}, {air, air, air}});
+
 		Ludus.mainWindow.setFloor(floor);
 	}
 }
