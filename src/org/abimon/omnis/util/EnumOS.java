825b19586d62eccae3f21b7e18d247926ca7d88c
Isaac <undermybrella@abimon.org>
2015-12-11 22:55:34 +1100

    Added a helper method to EnumOS
    Added a helper method to get a storage location for a program

--- src/org/abimon/omnis/util/EnumOS.java
+++ src/org/abimon/omnis/util/EnumOS.java
@@ -1,15 +1,30 @@
 package org.abimon.omnis.util;
 
+import java.io.File;
+
 public enum EnumOS {
-	WINDOWS,
-	MACOSX,
-	LINUX,
+	
+	WINDOWS(System.getProperty("user.home") + File.separator),
+	MACOSX(System.getProperty("user.home") + "/Library/Application Support/"),
+	LINUX(System.getProperty("user.home") + File.separator),
 	OTHER;
+	
+	String storageLocation = "Omnis";
+	
+	EnumOS(){}
+	
+	EnumOS(String storageLocation){
+		this.storageLocation = storageLocation;
+	}
 	
 	public static EnumOS determineOS(){
 		String os = System.getProperty("os.name").toLowerCase();
 		if(os.contains("mac"))
 			return MACOSX;
 		return OTHER;
+	}
+	
+	public File getStorageLocation(String folderName){
+		return new File(storageLocation + folderName);
 	}
 }
