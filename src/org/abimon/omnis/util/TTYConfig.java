828fb7adce0d504b28f90b3dd7992e2453fb3e50
Isaac <undermybrella@abimon.org>
2015-12-12 13:49:47 +1100

    Prevented the additional shutdown thread from being added if it was already added

--- src/org/abimon/omnis/util/TTYConfig.java
+++ src/org/abimon/omnis/util/TTYConfig.java
@@ -13,24 +13,25 @@ public class TTYConfig {
 	public static String ttyConfig = "";
 
 	public static void setTerminalToCBreak() throws IOException, InterruptedException {
-		
+
 		if(System.console() == null)
 			throw new IOException("No console object! Exiting for safety precautions");
 
-		ttyConfig = stty("-g");
-		
-		Runtime.getRuntime().addShutdownHook(new Thread(){
-			public void run(){
-				try {
-					stty(ttyConfig.trim());
-					System.out.println("Returned Stty");
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+		if(ttyConfig.equals(""))
+			Runtime.getRuntime().addShutdownHook(new Thread(){
+				public void run(){
+					try {
+						stty(ttyConfig.trim());
+						System.out.println("Returned Stty");
+					} catch (IOException e) {
+						e.printStackTrace();
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 				}
-			}
-		});
+			});
+		
+		ttyConfig = stty("-g");
 
 		// set the console to be character-buffered instead of line-buffered
 		stty("-icanon min 1");
@@ -38,9 +39,9 @@ public class TTYConfig {
 		// disable character echoing
 		stty("-echo");
 	}
-	
+
 	public static void setTerminalToLBreak() throws IOException, InterruptedException {
-		
+
 		if(System.console() == null)
 			throw new IOException("No console object! Exiting for safety precautions");
 
