7e344fe6fd2af26a3eff76bee33e9fbf3c0ff366
Isaac <undermybrella@abimon.org>
2015-12-23 09:36:48 +1100

    Allowed content to be retrieved from behind a proxy

--- src/org/abimon/omnis/net/Website.java
+++ src/org/abimon/omnis/net/Website.java
@@ -1,6 +1,7 @@
 package org.abimon.omnis.net;
 
 import java.io.InputStream;
+import java.net.Proxy;
 import java.net.URL;
 
 public class Website {
@@ -15,6 +16,20 @@ public class Website {
 	public String retrieveContent(){
 		try{
 			InputStream http = new URL(ip).openStream();
+			byte[] data = new byte[http.available()];
+			http.read(data);
+			http.close();
+			return new String(data);
+		}
+		catch(Throwable th){
+			th.printStackTrace();
+		}
+		return "";
+	}
+	
+	public String retrieveContent(Proxy proxy){
+		try{
+			InputStream http = new URL(ip).openConnection(proxy).getInputStream();
 			byte[] data = new byte[http.available()];
 			http.read(data);
 			http.close();
